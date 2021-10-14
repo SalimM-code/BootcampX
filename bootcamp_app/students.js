c8f0212583c5be515b29c28ebb7ec8244296a289
salimM-code <mustafasalim000@gmail.com>
2021-10-14 15:15:34 -0400

    implemented ways to prevent sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,3 +1,19 @@
+
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// store all potential malicious values in an array
+const values = [`%${cohortName}%`, limit]
+
+
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,14 +23,9 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
+pool  
+  .query(queryString, values)
+  .then(res => {
   console.log(res.rows)
-})
-.catch(err => console.error('query error', err.stack));
+  })
+  .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,21 @@
+const args = process.argv
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistance
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;
+`;
+
+const cohortName = args[2];
+const values = [`${cohortName || JUL02}`];
+
+
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -8,17 +26,9 @@ const pool = new Pool({
 });
 
 
-pool.query (`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistance
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || JUL02}'
-GROUP BY teachers.name, cohorts.name
-ORDER BY teachers.name;
-`)
-.then(res => {
+pool
+  .query (queryString, values)
+  .then(res => {
   console.log(res.rows)
-})
-.catch(err => console.error('query error', err.stack));
+  })
+  .catch(err => console.error('query error', err.stack));
